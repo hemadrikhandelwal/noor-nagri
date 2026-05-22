c01aa72e16a64b7d2495d0004270b9e9886405d8
hemadrikhandelwal <hemadrikhandelwal@gmail.com>
2026-05-22 15:34:22 +0530

    chore:application routing setup

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,52 @@
+import {  createBrowserRouter,RouterProvider } from "react-router-dom";
+import Home from "@/pages/Home";
+import Cart from "@/pages/Cart";
+import Products from "@/pages/Products";
+import ProductDetails from "@/pages/ProductDetails";
+import Wishlist from "@/pages/Wishlist";
+import NotFound from "@/pages/NotFound";
+
+const router = createBrowserRouter([
+
+     {
+        path:"/",
+         element:<Home /> 
+     },
+
+
+      {
+        path:"/cart",
+         element:<Cart /> 
+     },
+
+      {
+        path:"/products",
+         element:<Products /> 
+     },
+
+      {
+        path:"/product/:id",
+         element:<ProductDetails /> 
+     },
+
+      {
+        path:"/wishlist",
+         element:<Wishlist /> 
+     },
+      {
+        path:"*",
+         element:<NotFound /> 
+     },
+
+
+])
+
+const AppRoutes = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default AppRoutes;
+
+ 
+     
+  
