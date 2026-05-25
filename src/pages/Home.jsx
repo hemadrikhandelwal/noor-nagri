22a78b5c459d521cbe10d84940190c459e81791d
hemadrikhandelwal <hemadrikhandelwal@gmail.com>
2026-05-25 12:41:28 +0530

    api integration using axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,53 @@
+import ProductCard from "@/components/ui/ProductCard";
+import fetchAllProducts from "@/services/productservice";
+import { useEffect, useState } from "react";
+
 export default function Home(){
+    const[products,setProducts] = useState([]); 
+    const[isLoading,setLoading] = useState(true);
+    const[isError, setError] = useState(null);
+
+    const fetchProducts = async ()=>{
+        try{
+            const response = await fetchAllProducts('/products');
+            setProducts(response.data.products);
+            }
+        catch(error){
+                setError(error);
+            }
+        finally{
+            setLoading(false)
+        }
+        }
+
+        useEffect(()=>{
+            fetchProducts();
+        },[]);
+
+        if (isLoading) {
+        return <p>Loading...</p>;
+        }
+
+        // Error UI
+        if (isError) {
+            return <p>Something went wrong!</p>;
+        }
+
     return(
         <>
-        <h1>Home Page</h1>
+       
+        {
+            products.map((product)=>{
+                return <ProductCard key = {product.id} product = {product}/>
+
+            })
+        }
+
+           
+            
+        
+        
+        
         </>
     )
 }
